7d7344cf0e408dff954d6eda9f909b7b3a080f46
Christophe Grand (cgrand) <christophe@cgrand.net>
2009-04-17 16:20:03 +0200

    Reordered declarations, removed unused 'accept

--- src/net/cgrand/enlive_html.clj
+++ src/net/cgrand/enlive_html.clj
@@ -116,6 +116,11 @@
 (defn flatmap [f xs]
   (flatten (map f xs)))
 
+(defn attr-values 
+ "Returns the whitespace-separated values of the specified attr as a set."
+ [node attr]
+  (set (-> node :attrs (attr "") (.split "\\s+"))))
+
 ;; state machine stuff
 ;; a state is a pair consisting of a boolean (acceptance) and a seq of functions (functions from loc to state)
 
@@ -155,8 +160,142 @@
 (def descendants-or-self
   [true (lazy-seq [(constantly descendants-or-self)])])
 
-(def accept [true nil])
 
+;; selector syntax
+(defn- simplify-associative [[op & forms]]
+  (if (next forms)
+    (cons op (mapcat #(if (and (seq? %) (= op (first %))) (rest %) (list %)) forms)) 
+    (first forms)))
+
+(defn- emit-union [forms]
+  (simplify-associative (cons `union forms)))
+
+(defn- emit-intersection [forms]
+  (simplify-associative (cons `intersection forms)))
+
+(defn- emit-chain [forms]
+  (simplify-associative (cons `chain forms)))
+
+(defn- compile-keyword [kw]
+  (let [[tag-name & etc] (.split (name kw) "(?=[#.])")
+        tag-pred (if (#{"" "*"} tag-name) [] [`(tag= ~(keyword tag-name))])
+        ids-pred (for [s etc :when (= \# (first s))] `(id= ~(subs s 1)))
+        classes (set (for [s etc :when (= \. (first s))] (subs s 1)))
+        class-pred (when (seq classes) [`(has-class ~@classes)])] 
+    (emit-intersection (concat tag-pred ids-pred class-pred))))
+    
+(declare compile-step)
+
+(defn- compile-union [s]
+  (emit-union (map compile-step s)))      
+    
+(defn- compile-intersection [s]
+  (emit-intersection (map compile-step s)))      
+
+(defn- compile-step [s]
+  (cond
+    (keyword? s) (compile-keyword s)    
+    (set? s) (compile-union s)    
+    (vector? s) (compile-intersection s)
+    :else s))
+
+(defn- compile-chain [s]
+  (let [[child-ops [step & next-steps :as steps]] (split-with #{:>} s)
+        next-chain (when (seq steps)
+                     (if (seq next-steps)
+                       (emit-chain [(compile-step step) (compile-chain next-steps)])
+                       (compile-step step)))]
+    (if (seq child-ops)
+      next-chain      
+      (emit-chain [`descendants-or-self next-chain])))) 
+
+(defn compile-selector [s]
+  (if (set? s)
+    (emit-union (map compile-selector s)) 
+    (compile-chain s)))
+
+;; core 
+  
+(defn- children-locs [loc]
+  (take-while identity (iterate z/right (z/down loc))))
+
+(defn- transform-loc [loc previous-state transformation]
+  (if (z/branch? loc)
+    (let [state (step previous-state loc)
+          children (flatmap #(transform-loc % state transformation) (children-locs loc))
+          node (if (= children (z/children loc)) 
+                 (z/node loc) 
+                 (z/make-node loc (z/node loc) children))]
+      (if (accept? state)
+        (transformation node)
+        node))
+    (z/node loc)))
+      
+(defn transform [nodes [state transformation]]
+  (flatmap #(transform-loc (z/xml-zip %) state transformation) nodes))
+
+(defn at* [nodes & rules]
+  (reduce transform nodes (partition 2 rules)))
+
+(defmacro selector
+ "Turns the selector into clojure code." 
+ [selector]
+  (compile-selector selector))
+
+(defmacro at [node & rules]
+  `(at* [~node] ~@(map #(%1 %2) (cycle [#(list `selector %) identity]) rules)))
+
+(defn select* [nodes state]
+  (let [select1 
+         (fn select1 [loc previous-state] 
+           (let [state (step previous-state loc)]
+             (if (accept? state)
+               (list (z/node loc))
+               (mapcat #(select1 % state) (children-locs loc)))))]
+    (mapcat #(select1 (z/xml-zip %) state) nodes)))
+      
+(defmacro select
+ "Returns the seq of nodes and sub-nodes matched by the specified selector."
+ [nodes selector]
+  `(select* ~nodes (selector ~selector)))
+
+;; main macros
+(defmacro snippet* [nodes args & forms]
+  (let [transform (if (next forms) `(fn [node#] (at node# ~@forms)) (first forms))]
+    `(let [nodes# ~nodes]
+       (fn ~args
+         (flatmap ~transform nodes#)))))
+    
+(defmacro snippet 
+ "A snippet is a function that returns a seq of nodes."
+ [source selector args & forms]
+  `(snippet* (select (html-resource ~source) ~selector) ~args ~@forms))  
+
+(defmacro template 
+ "A template returns a seq of string."
+ ([source args & forms]
+   `(comp emit* (snippet* (html-resource ~source) ~args ~@forms))))
+
+(defmacro defsnippet
+ "Define a named snippet -- equivalent to (def name (snippet source selector args ...))."
+ [name source selector args & forms]
+ `(def ~name (snippet ~source ~selector ~args ~@forms)))
+   
+(defmacro deftemplate
+ "Defines a template as a function that returns a seq of strings." 
+ [name source args & forms] 
+  `(def ~name (template ~source ~args ~@forms)))
+
+(defmacro defsnippets
+ [source & specs]
+  (let [xml (html-resource source)]
+   `(do
+     ~@(map (fn [[name selector args & forms]]
+              `(def ~name (snippet ~xml ~selector ~args ~@forms)))
+         specs))))
+
+
+;; predicates utils
 (defn pred 
  "Turns a predicate function on elements into a predicate-step usable in selectors."
  [f]
@@ -169,6 +308,7 @@
   [false [(fn [loc]
             [(and (z/branch? loc) (f loc)) nil])]])
 
+;; predicates
 (defn tag= 
  "Selector predicate, :foo is as short-hand for (tag= :foo)."
  [tag-name]
@@ -178,11 +318,6 @@
  "Selector predicate, :#foo is as short-hand for (id= \"foo\")."
  [id]
   (pred #(= (-> % :attrs :id) id)))
-
-(defn attr-values 
- "Returns the whitespace-separated values of the specified attr as a set."
- [node attr]
-  (set (-> node :attrs (attr "") (.split "\\s+"))))
 
 (defn attr? 
  "Selector predicate, tests if the specified attributes are present."
@@ -299,103 +434,6 @@
 
 (def even (nth-child 2 0))
 
-;; selector syntax
-(defn- simplify-associative [[op & forms]]
-  (if (next forms)
-    (cons op (mapcat #(if (and (seq? %) (= op (first %))) (rest %) (list %)) forms)) 
-    (first forms)))
-
-(defn- emit-union [forms]
-  (simplify-associative (cons `union forms)))
-
-(defn- emit-intersection [forms]
-  (simplify-associative (cons `intersection forms)))
-
-(defn- emit-chain [forms]
-  (simplify-associative (cons `chain forms)))
-
-(defn- compile-keyword [kw]
-  (let [[tag-name & etc] (.split (name kw) "(?=[#.])")
-        tag-pred (if (#{"" "*"} tag-name) [] [`(tag= ~(keyword tag-name))])
-        ids-pred (for [s etc :when (= \# (first s))] `(id= ~(subs s 1)))
-        classes (set (for [s etc :when (= \. (first s))] (subs s 1)))
-        class-pred (when (seq classes) [`(has-class ~@classes)])] 
-    (emit-intersection (concat tag-pred ids-pred class-pred))))
-    
-(declare compile-step)
-
-(defn- compile-union [s]
-  (emit-union (map compile-step s)))      
-    
-(defn- compile-intersection [s]
-  (emit-intersection (map compile-step s)))      
-
-(defn- compile-step [s]
-  (cond
-    (keyword? s) (compile-keyword s)    
-    (set? s) (compile-union s)    
-    (vector? s) (compile-intersection s)
-    :else s))
-
-(defn- compile-chain [s]
-  (let [[child-ops [step & next-steps :as steps]] (split-with #{:>} s)
-        next-chain (when (seq steps)
-                     (if (seq next-steps)
-                       (emit-chain [(compile-step step) (compile-chain next-steps)])
-                       (compile-step step)))]
-    (if (seq child-ops)
-      next-chain      
-      (emit-chain [`descendants-or-self next-chain])))) 
-
-(defn compile-selector [s]
-  (if (set? s)
-    (emit-union (map compile-selector s)) 
-    (compile-chain s)))
-
-;; core 
-  
-(defn- children-locs [loc]
-  (take-while identity (iterate z/right (z/down loc))))
-
-(defn- transform-loc [loc previous-state transformation]
-  (if (z/branch? loc)
-    (let [state (step previous-state loc)
-          children (flatmap #(transform-loc % state transformation) (children-locs loc))
-          node (if (= children (z/children loc)) 
-                 (z/node loc) 
-                 (z/make-node loc (z/node loc) children))]
-      (if (accept? state)
-        (transformation node)
-        node))
-    (z/node loc)))
-      
-(defn transform [nodes [state transformation]]
-  (flatmap #(transform-loc (z/xml-zip %) state transformation) nodes))
-
-(defn at* [nodes & rules]
-  (reduce transform nodes (partition 2 rules)))
-
-(defmacro selector
- "Turns the selector into clojure code." 
- [selector]
-  (compile-selector selector))
-
-(defmacro at [node & rules]
-  `(at* [~node] ~@(map #(%1 %2) (cycle [#(list `selector %) identity]) rules)))
-
-(defn select* [nodes state]
-  (let [select1 
-         (fn select1 [loc previous-state] 
-           (let [state (step previous-state loc)]
-             (if (accept? state)
-               (list (z/node loc))
-               (mapcat #(select1 % state) (children-locs loc)))))]
-    (mapcat #(select1 (z/xml-zip %) state) nodes)))
-      
-(defmacro select
- "Returns the seq of nodes and sub-nodes matched by the specified selector."
- [nodes selector]
-  `(select* ~nodes (selector ~selector)))
 
 ;; transformations
 
@@ -444,38 +482,3 @@
  "Chains (composes) several transformations. Applies functions from left to right." 
  [& fns]
   #(reduce (fn [nodes f] (flatmap f nodes)) [%] fns))
-
-;; main macros
-(defmacro snippet* [nodes args & forms]
-  (let [transform (if (next forms) `(fn [node#] (at node# ~@forms)) (first forms))]
-    `(let [nodes# ~nodes]
-       (fn ~args
-         (flatmap ~transform nodes#)))))
-    
-(defmacro snippet 
- "A snippet is a function that returns a seq of nodes."
- [source selector args & forms]
-  `(snippet* (select (html-resource ~source) ~selector) ~args ~@forms))  
-
-(defmacro template 
- "A template returns a seq of string."
- ([source args & forms]
-   `(comp emit* (snippet* (html-resource ~source) ~args ~@forms))))
-
-(defmacro defsnippet
- "Define a named snippet -- equivalent to (def name (snippet source selector args ...))."
- [name source selector args & forms]
- `(def ~name (snippet ~source ~selector ~args ~@forms)))
-   
-(defmacro deftemplate
- "Defines a template as a function that returns a seq of strings." 
- [name source args & forms] 
-  `(def ~name (template ~source ~args ~@forms)))
-
-(defmacro defsnippets
- [source & specs]
-  (let [xml (html-resource source)]
-   `(do
-     ~@(map (fn [[name selector args & forms]]
-              `(def ~name (snippet ~xml ~selector ~args ~@forms)))
-         specs))))
