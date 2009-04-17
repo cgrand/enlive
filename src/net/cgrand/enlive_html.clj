4c2cbe69a85bf76629ae84fdfa8dae2fe9d9b8ac
Christophe Grand (cgrand) <christophe@cgrand.net>
2009-04-17 18:35:25 +0200

    Added some tests and fixed attr-values.

--- src/net/cgrand/enlive_html.clj
+++ src/net/cgrand/enlive_html.clj
@@ -12,7 +12,7 @@
   (:refer-clojure :exclude [empty])
   (:require [clojure.xml :as xml])
   (:require [clojure.zip :as z])
-  (:use [clojure.contrib.test-is :as test-is :only [set-test with-test is]]))
+  (:use [clojure.contrib.test-is :as test-is :only [set-test with-test is are]]))
 
 ;; enlive-html is a selector-based templating engine
 ;;
@@ -119,7 +119,7 @@
 (defn attr-values 
  "Returns the whitespace-separated values of the specified attr as a set."
  [node attr]
-  (set (-> node :attrs (attr "") (.split "\\s+"))))
+  (disj (set (-> node :attrs (attr "") (.split "\\s+"))) ""))
 
 ;; state machine stuff
 ;; a state is a pair consisting of a boolean (acceptance) and a seq of functions (functions from loc to state)
@@ -294,146 +294,40 @@
               `(def ~name (snippet ~xml ~selector ~args ~@forms)))
          specs))))
 
+;; test utilities
+(defn- htmlize [node]
+  (cond
+    (map? node)
+      (-> node
+        (assoc-in [:attrs :class] (attr-values node :class))
+        (update-in [:content] htmlize))
+    (or (coll? node) (seq? node))
+      (map htmlize node)
+    :else node))
 
-;; predicates utils
-(defn pred 
- "Turns a predicate function on elements into a predicate-step usable in selectors."
- [f]
-  [false [(fn [loc]
-            [(and (z/branch? loc) (f (z/node loc))) nil])]])
+(defn- html [s]
+  (htmlize 
+    (if (string? s)
+      (html-resource (java.io.StringReader. s))
+      s))) 
 
-(defn loc-pred 
- "Turns a predicate function on locs (see clojure.core.zip) into a predicate-step usable in selectors."
- [f]
-  [false [(fn [loc]
-            [(and (z/branch? loc) (f loc)) nil])]])
+(defn- src [s]
+  (first (html-resource (java.io.StringReader. s))))
 
-;; predicates
-(defn tag= 
- "Selector predicate, :foo is as short-hand for (tag= :foo)."
- [tag-name]
-  (pred #(= (:tag %) tag-name)))
+(defn- same? [& xs]
+  (apply = (map html xs)))
 
-(defn id=
- "Selector predicate, :#foo is as short-hand for (id= \"foo\")."
- [id]
-  (pred #(= (-> % :attrs :id) id)))
+(defn- elt 
+ ([tag] (elt tag nil))
+ ([tag attrs & content]
+   {:tag tag
+    :attrs attrs
+    :content content}))
 
-(defn attr? 
- "Selector predicate, tests if the specified attributes are present."
- [& kws]
-  (pred #(every? (-> % :attrs keys set) kws)))
-  
-(defn- every?+ [pred & colls]
-  (every? #(apply pred %) (apply map vector colls))) 
-
-(defn- multi-attr-pred 
- [single-attr-pred]
-  (fn [& kvs]
-    (let [ks (take-nth 2 kvs)
-          vs (take-nth 2 (rest kvs))]
-      (pred #(when-let [attrs (:attrs %)]
-               (every?+ single-attr-pred (map attrs ks) vs))))))           
-
-(def #^{:doc "Selector predicate, tests if the specified attributes have the specified values."} 
- attr= 
-  (multi-attr-pred =))
-
-(defn attr-has
- "Selector predicate, tests if the specified whitespace-seperated attribute contains the specified values. See CSS ~="
- [attr & values]
-  (pred #(every? (attr-values % attr) values)))
- 
-(defn has-class 
- "Selector predicate, :.foo.bar is as short-hand for (has-class \"foo\" \"bar\")."
- [& classes]
-  (apply attr-has :class classes)) 
-
-(defn- starts-with? [#^String s #^String prefix]
-  (and s (.startsWith s prefix)))
-
-(defn- ends-with? [#^String s #^String suffix]
-  (and s (.endsWith s suffix)))
-
-(defn- contains-substring? [#^String s #^String substring]
-  (and s (<= 0 (.indexOf s substring))))
-
-(def #^{:doc "Selector predicate, tests if the specified attributes start with the specified values. See CSS ^= ."} 
- attr-starts
-  (multi-attr-pred starts-with?))
-
-(def #^{:doc "Selector predicate, tests if the specified attributes end with the specified values. See CSS $= ."} 
- attr-ends
-  (multi-attr-pred ends-with?))
-
-(def #^{:doc "Selector predicate, tests if the specified attributes contain the specified values. See CSS *= ."} 
- attr-contains
-  (multi-attr-pred contains-substring?))
-
-(defn- is-first-segment? [#^String s #^String segment]
-  (and s 
-    (.startsWith s segment)
-    (= \- (.charAt s (count segment)))))
-             
-(def #^{:doc "Selector predicate, tests if the specified attributes start with the specified values. See CSS |= ."}
- attr|=           
-  (multi-attr-pred is-first-segment?))
-
-(def root 
-  (loc-pred #(-> % z/up nil?)))
-
-(defn- nth? 
- [f a b]
-  (if (zero? a)
-    #(= (-> (f %) count inc) b)
-    #(let [an+b (-> (filter map? (f %)) count inc)
-           an (- an+b b)]
-       (and (zero? (rem an a)) (<= 0 (quot an a))))))
-      
-(defn nth-child
- "Selector step, tests if the node has an+b-1 siblings on its left. See CSS :nth-child."
- ([b] (nth-child 0 b))
- ([a b] (loc-pred (nth? z/lefts a b))))
-      
-(defn nth-last-child
- "Selector step, tests if the node has an+b-1 siblings on its right. See CSS :nth-last-child."
- ([b] (nth-last-child 0 b))
- ([a b] (loc-pred (nth? z/rights a b))))
-
-(defn- filter-of-type [f]
-  (fn [loc]
-    (let [tag (-> loc z/node :tag)
-          pred #(= (:tag %) tag)]
-      (filter pred (f loc)))))
-
-(defn nth-of-type
- "Selector step, tests if the node has an+b-1 siblings of the same type (tag name) on its left. See CSS :nth-of-type."
- ([b] (nth-of-type 0 b))
- ([a b] (loc-pred (nth? (filter-of-type z/lefts) a b))))
-
-(defn nth-last-of-type
- "Selector step, tests if the node has an+b-1 siblings of the same type (tag name) on its right. See CSS :nth-last-of-type."
- ([b] (nth-last-of-type 0 b))
- ([a b] (loc-pred (nth? (filter-of-type z/rights) a b))))
-
-(def first-child (nth-child 1))      
-      
-(def last-child (nth-last-child 1))      
-      
-(def first-of-type (nth-of-type 1))      
-      
-(def last-of-type (nth-last-of-type 1))      
-
-(def only-child (intersection first-child last-child))  
-
-(def only-of-type (intersection first-of-type last-of-type))
-
-(def empty (pred #(empty? (remove empty? (:content %)))))
-
-(def odd (nth-child 2 1))
-
-(def even (nth-child 2 0))
-
+(defmacro #^{:private true} 
+ is-same
+ [& forms]
+ `(is (same? ~@forms)))
 
 ;; transformations
 
@@ -482,3 +376,204 @@
  "Chains (composes) several transformations. Applies functions from left to right." 
  [& fns]
   #(reduce (fn [nodes f] (flatmap f nodes)) [%] fns))
+
+
+;; predicates utils
+(defn pred 
+ "Turns a predicate function on elements into a predicate-step usable in selectors."
+ [f]
+  [false [(fn [loc]
+            [(and (z/branch? loc) (f (z/node loc))) nil])]])
+
+(defn loc-pred 
+ "Turns a predicate function on locs (see clojure.core.zip) into a predicate-step usable in selectors."
+ [f]
+  [false [(fn [loc]
+            [(and (z/branch? loc) (f loc)) nil])]])
+
+;; predicates
+(defn- test-step [expected state node]
+  (= expected (boolean (accept? (step state (z/xml-zip node))))))
+
+(with-test
+  (defn tag= 
+   "Selector predicate, :foo is as short-hand for (tag= :foo)."
+   [tag-name]
+    (pred #(= (:tag %) tag-name)))
+    
+  (are (test-step _1 _2 _3)
+    true (tag= :foo) (elt :foo)
+    false (tag= :bar) (elt :foo)))
+
+(with-test
+  (defn id=
+   "Selector predicate, :#foo is as short-hand for (id= \"foo\")."
+   [id]
+    (pred #(= (-> % :attrs :id) id)))
+
+  (are (test-step _1 _2 _3)
+    true (id= "foo") (elt :div {:id "foo"})
+    false (id= "bar") (elt :div {:id "foo"})
+    false (id= "foo") (elt :div)))
+
+(with-test  
+  (defn attr? 
+   "Selector predicate, tests if the specified attributes are present."
+   [& kws]
+    (pred #(every? (-> % :attrs keys set) kws)))
+
+  (are (test-step _1 _2 _3)
+    true (attr? :href) (elt :a {:href "http://cgrand.net/"})
+    false (attr? :href) (elt :a {:name "toc"})
+    false (attr? :href :title) (elt :a {:href "http://cgrand.net/"})
+    true (attr? :href :title) (elt :a {:href "http://cgrand.net/" :title "home"})))
+  
+(defn- every?+ [pred & colls]
+  (every? #(apply pred %) (apply map vector colls))) 
+
+(defn- multi-attr-pred 
+ [single-attr-pred]
+  (fn [& kvs]
+    (let [ks (take-nth 2 kvs)
+          vs (take-nth 2 (rest kvs))]
+      (pred #(when-let [attrs (:attrs %)]
+               (every?+ single-attr-pred (map attrs ks) vs))))))           
+
+(with-test
+  (def #^{:doc "Selector predicate, tests if the specified attributes have the specified values."} 
+   attr= 
+    (multi-attr-pred =))
+    
+  (are (test-step _1 _2 (elt :a {:href "http://cgrand.net/" :title "home"}))
+    true (attr= :href "http://cgrand.net/")
+    false (attr= :href "http://clojure.org/")
+    false (attr= :href "http://cgrand.net/" :name "home") 
+    false (attr= :href "http://cgrand.net/" :title "homepage")
+    true (attr= :href "http://cgrand.net/" :title "home")))
+
+(defn attr-has
+ "Selector predicate, tests if the specified whitespace-seperated attribute contains the specified values. See CSS ~="
+ [attr & values]
+  (pred #(every? (attr-values % attr) values)))
+ 
+(defn has-class 
+ "Selector predicate, :.foo.bar is as short-hand for (has-class \"foo\" \"bar\")."
+ [& classes]
+  (apply attr-has :class classes)) 
+
+(defn- starts-with? [#^String s #^String prefix]
+  (and s (.startsWith s prefix)))
+
+(defn- ends-with? [#^String s #^String suffix]
+  (and s (.endsWith s suffix)))
+
+(defn- contains-substring? [#^String s #^String substring]
+  (and s (<= 0 (.indexOf s substring))))
+
+(with-test
+  (def #^{:doc "Selector predicate, tests if the specified attributes start with the specified values. See CSS ^= ."} 
+   attr-starts
+    (multi-attr-pred starts-with?))
+
+  (are (test-step _1 _2 (elt :a {:href "http://cgrand.net/" :title "home"}))
+    true (attr-starts :href "http://cgr")
+    false (attr-starts :href "http://clo")
+    false (attr-starts :href "http://cgr" :name "ho")
+    false (attr-starts :href "http://cgr" :title "x") 
+    true (attr-starts :href "http://cgr" :title "ho")))
+
+(with-test
+  (def #^{:doc "Selector predicate, tests if the specified attributes end with the specified values. See CSS $= ."} 
+   attr-ends
+    (multi-attr-pred ends-with?))
+
+  (are (test-step _1 _2 (elt :a {:href "http://cgrand.net/" :title "home"}))
+    true (attr-ends :href "d.net/")
+    false (attr-ends :href "e.org/")
+    false (attr-ends :href "d.net/" :name "me")
+    false (attr-ends :href "d.net/" :title "hom")
+    true (attr-ends :href "d.net/" :title "me")))
+
+(with-test
+  (def #^{:doc "Selector predicate, tests if the specified attributes contain the specified values. See CSS *= ."} 
+   attr-contains
+    (multi-attr-pred contains-substring?))
+    
+  (are (test-step _1 _2 (elt :a {:href "http://cgrand.net/" :title "home"}))
+    true (attr-contains :href "rand")
+    false (attr-contains :href "jure")
+    false (attr-contains :href "rand" :name "om") 
+    false (attr-contains :href "rand" :title "pa")
+    true (attr-contains :href "rand" :title "om")))
+
+(defn- is-first-segment? [#^String s #^String segment]
+  (and s 
+    (.startsWith s segment)
+    (= \- (.charAt s (count segment)))))
+             
+(def #^{:doc "Selector predicate, tests if the specified attributes start with the specified values. See CSS |= ."}
+ attr|=           
+  (multi-attr-pred is-first-segment?))
+
+(def root 
+  (loc-pred #(-> % z/up nil?)))
+
+(defn- nth? 
+ [f a b]
+  (if (zero? a)
+    #(= (-> (f %) count inc) b)
+    #(let [an+b (-> (filter map? (f %)) count inc)
+           an (- an+b b)]
+       (and (zero? (rem an a)) (<= 0 (quot an a))))))
+
+(with-test      
+  (defn nth-child
+   "Selector step, tests if the node has an+b-1 siblings on its left. See CSS :nth-child."
+   ([b] (nth-child 0 b))
+   ([a b] (loc-pred (nth? z/lefts a b))))
+
+  (are (same? _2 (at (src "<dl><dt>1<dt>2<dt>3<dt>4<dt>5") _1 (add-class "foo")))    
+    [[:dt (nth-child 2)]] "<dl><dt>1<dt class=foo>2<dt>3<dt>4<dt>5" 
+    [[:dt (nth-child 2 0)]] "<dl><dt>1<dt class=foo>2<dt>3<dt class=foo>4<dt>5" 
+    [[:dt (nth-child 3 1)]] "<dl><dt class=foo>1<dt>2<dt>3<dt class=foo>4<dt>5" 
+    [[:dt (nth-child -1 3)]] "<dl><dt class=foo>1<dt class=foo>2<dt class=foo>3<dt>4<dt>5" 
+    [[:dt (nth-child 3 -1)]] "<dl><dt>1<dt class=foo>2<dt>3<dt>4<dt class=foo>5"))
+      
+(defn nth-last-child
+ "Selector step, tests if the node has an+b-1 siblings on its right. See CSS :nth-last-child."
+ ([b] (nth-last-child 0 b))
+ ([a b] (loc-pred (nth? z/rights a b))))
+
+(defn- filter-of-type [f]
+  (fn [loc]
+    (let [tag (-> loc z/node :tag)
+          pred #(= (:tag %) tag)]
+      (filter pred (f loc)))))
+
+(defn nth-of-type
+ "Selector step, tests if the node has an+b-1 siblings of the same type (tag name) on its left. See CSS :nth-of-type."
+ ([b] (nth-of-type 0 b))
+ ([a b] (loc-pred (nth? (filter-of-type z/lefts) a b))))
+
+(defn nth-last-of-type
+ "Selector step, tests if the node has an+b-1 siblings of the same type (tag name) on its right. See CSS :nth-last-of-type."
+ ([b] (nth-last-of-type 0 b))
+ ([a b] (loc-pred (nth? (filter-of-type z/rights) a b))))
+
+(def first-child (nth-child 1))      
+      
+(def last-child (nth-last-child 1))      
+      
+(def first-of-type (nth-of-type 1))      
+      
+(def last-of-type (nth-last-of-type 1))      
+
+(def only-child (intersection first-child last-child))  
+
+(def only-of-type (intersection first-of-type last-of-type))
+
+(def empty (pred #(empty? (remove empty? (:content %)))))
+
+(def odd (nth-child 2 1))
+
+(def even (nth-child 2 0))
