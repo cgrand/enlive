79cd1a11209aeab12b67bb3fea5fb742efcdbbc6
Christophe Grand <christophe@cgrand.net>
2010-01-26 10:18:04 +0100

    leinification

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject enlive "1.0.0-SNAPSHOT"
+  :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
+  :url "http://github.com/cgrand/enlive/"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.ccil.cowan.tagsoup/tagsoup "1.2"]])
