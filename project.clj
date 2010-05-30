8fe69e7f30a8793435f735c15eab4e66a3be3c1f
Roman Scherer <roman@soundcloud.com>
2010-05-30 21:13:04 +0200

    Use clojure 1.2 master branch.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject enlive "1.0.0-SNAPSHOT"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
-  :dependencies [[org.clojure/clojure "[1.1.0,)"]
-                 [org.ccil.cowan.tagsoup/tagsoup "1.2"]])
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.ccil.cowan.tagsoup/tagsoup "1.2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
