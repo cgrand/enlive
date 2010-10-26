ea5d96576a5855dc369a8c37ce6ec2ae1cf635e0
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-10-27 03:03:53 +0900

    Use clojure 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject enlive "1.0.0-SNAPSHOT"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
