616fd90ef13aa27b072ddec7488d188218c03831
Christophe Grand <christophe@cgrand.net>
2012-06-04 15:25:46 +0200

    v1.0.1 mainly fix the laziness issue

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject enlive "1.2.0-alpha1"
+(defproject enlive "1.0.1"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
