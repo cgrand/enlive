92e93c3cfc56b3f0db0df491fa572dc8e3dd8c63
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-01-09 15:33:04 +0100

    Updating tagsoup to 1.2.1

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
-                 [org.ccil.cowan.tagsoup/tagsoup "1.2"]])
+                 [org.ccil.cowan.tagsoup/tagsoup "1.2.1"]])
