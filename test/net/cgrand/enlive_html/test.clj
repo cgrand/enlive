92620a582475da8d87e447ff734dbd719f4d7fec
Christophe Grand <christophe@cgrand.net>
2013-06-25 21:54:19 +0100

    fix #67

--- test/net/cgrand/enlive_html/test.clj
+++ test/net/cgrand/enlive_html/test.clj
@@ -290,5 +290,7 @@
 (deftest replace-vars-test
   (is-same "<div><h1>untouched ${name}<p class=hello>hello world"
     (sniptest "<div><<h1>untouched ${name}<p class=\"${class}\">hello ${name}"
-      #{[:p] [:p any-node]} (replace-vars {:name "world" :class "hello"}))))
+      #{[:p] [:p any-node]} (replace-vars {:name "world" :class "hello"})))
+  (is (= ((replace-vars {:a "A" :b "B"}) "${a} ${b}")
+        "A B")))
 
