228d80653e6f508c1b7719cecb84e2a58f52cf4c
Nikolay Matyushev <egao1980@gmail.com>
2014-05-15 14:49:55 +0100

    Fix reload on Windows machines

--- src/net/cgrand/reload.clj
+++ src/net/cgrand/reload.clj
@@ -44,10 +44,12 @@
                         (.register (.getParent path) ws 
                           (into-array [java.nio.file.StandardWatchEventKinds/ENTRY_MODIFY]))))))))))
 
-(def ^:private no-strings (into-array String nil))
+(defn- as-path [^java.net.URL url]
+  (let [uri (java.net.URI. (.toExternalForm url))
+        file (java.io.File. uri)]
+    (.toPath file)))
 
 (defmethod watch-url "file" [^java.net.URL url ns]
-  (let [fs (java.nio.file.FileSystems/getDefault)
-        path (.getPath fs (.getPath url) no-strings)
+  (let [path (as-path url)
         ws (watch-service ns)]
     (ws path)))
