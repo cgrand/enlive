8ece2dfe865ba05ee0e9bcb16092698fa8681468
egao1980 <egao1980@gmail.com>
2014-07-14 17:45:40 +0100

    Rolling back project file to create pull request

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject co.uk.egao/enlive "1.1.5"
   :min-lein-version "2.0.0"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
-  :url "http://github.com/egao1980/enlive"
+  :url "http://github.com/cgrand/enlive/"
   :profiles     {:dev {:resource-paths ["test/resources"]}}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2.1"]
