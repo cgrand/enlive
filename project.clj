891a147ccc75e6a6f6fc878832cafca542ab5b37
Alan Thompson <cloojure@gmail.com>
2019-03-02 11:31:23 -0800

    misc

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject enlive "1.1.6"
+(defproject enlive "1.1.7"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
   :min-lein-version "2.7.1"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2.1"]
-                 [org.jsoup/jsoup "1.11.3"]
-                 [tupelo "0.9.133"]]
+                 [org.jsoup/jsoup "1.11.3"]]
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]]}
              :dev      {:dependencies [[org.clojure/clojure "1.10.0"]]}
